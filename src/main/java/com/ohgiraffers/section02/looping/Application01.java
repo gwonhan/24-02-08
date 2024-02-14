456b539967cbf56864a50cbd2d9970631c56f3f6
gwonhan <h2odream21@gmail.com>
2024-02-14 17:16:44 +0900

    [Third comit] 24/02/14

--- src/main/java/com/ohgiraffers/section02/looping/Application01.java
+++ src/main/java/com/ohgiraffers/section02/looping/Application01.java
@@ -2,16 +2,21 @@ package com.ohgiraffers.section02.looping;
 
 public class Application01 {
     public static void main(String[] args) {
-     A_for a = new A_for();
+    // A_for a = new A_for();
   //  a.testSimpleForStatement();
 
 //         a.testExample4();
-    A_nesteFor b =new A_nesteFor();
+//    A_nesteFor b =new A_nesteFor();
     //b.printUpgradeGugudan();
-        b.printStarInputRowTimes();
+//        b.printStarInputRowTimes();
 
-
-
+//        B_while b = new B_while();
+//        b.testSimpleWhileStatement();
+//        b.testWhileExample1();
+//          b.testWhileExample2();
+    B_doWhile c = new B_doWhile();
+//    c.testSimpleDoWhileStatement();
+c.testSimpleDoWhileExample();
 
 
 
---
+++ src/main/java/com/ohgiraffers/section03/branching/Application01.java
@@ -0,0 +1,15 @@
+package com.ohgiraffers.section03.branching;
+
+import com.ohgiraffers.section02.looping.B_while;
+
+public class Application01 {
+    public static void main(String[] args) {
+
+//        A_break a = new A_break();
+//       a.testSimpleBreakStatement();
+        B_continue b = new  B_continue();
+       // b.testSimpleContinueStatement();
+        b.testSimpleContinueStatement2();
+    }
+
+}
